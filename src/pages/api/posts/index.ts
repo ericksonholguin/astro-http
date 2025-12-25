534f9889572938a00c6aa659e14d181e36311cd9
Erickson Manuel Holguin <erickson.holguin@fichap.com>
2025-12-25 08:02:25 -0400

    Refactor content configuration and enhance API error handling
    - Standardized quotation marks in src/content.config.ts for consistency.
    - Improved error handling in src/pages/api/posts/index.ts by returning a 404 response when a slug is not provided.
    - Updated API to use getEntry instead of getCollection for fetching blog posts.

--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -1,5 +1,5 @@
 import type { APIRoute } from "astro";
-import { getCollection } from "astro:content";
+import { getCollection, getEntry } from "astro:content";
 
 export const prerender = false;
 
@@ -7,9 +7,14 @@ export const GET: APIRoute = async ({ request, params }) => {
   const url = new URL(request.url);
   const slug = url.searchParams.get("slug");
 
-  console.log(url);
+  if (!slug) {
+    return new Response(JSON.stringify({ message: "Post not found" }), {
+      status: 404,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
 
-  const posts = await getCollection("blog", ({ id }) => slug === id);
+  const posts = await getEntry("blog", slug);
 
   return new Response(JSON.stringify(posts), {
     status: 200,
